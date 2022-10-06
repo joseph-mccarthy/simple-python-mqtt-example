ca5816f59a2a931ebf9c09a91640ad70afface58
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2022-10-06 15:56:03 +0100

    fix requirements and some comments, show sub topics

--- src/publish.py
+++ src/publish.py
@@ -3,17 +3,16 @@ import paho.mqtt.client as paho # import the mqtt library (pip3 install paho-mqt
 import random # import to generage random numbers
 
 def on_connect_callback(client,userdata,flags,rc): # define the on_connect callback method
-    print("connection callback...")
+    pass 
 
 def on_publish_callback(client, userdata, mid): # define the on_publish callback method
-    print("published data...")
+    pass
 
 
-broker = "broker.hivemq.com" # free mqtt broker
-topic = "jbm-example" # example topic name
+broker = "broker" # broker address
+topic = "examples/random_number" # example topic name with subtopic or random_number
 
-client = paho.Client(client_id="publisher-001-jbm-example", # id of this client sending data
-                        transport="tcp", ## protocol
+client = paho.Client(client_id="publisher-001-example", # id of this client sending data
                         reconnect_on_failure= True ## attempt to reconnect on failure
                         )
 
@@ -23,6 +22,7 @@ client.on_publish=on_publish_callback # assign callback method to the on_publish
 client.connect(broker) # connect to the broker
 
 while True: # infinite loop to keep publishing a random number
-    random_number = random.randint(1,100) # generate a random number between 1 and 100
+    random_number = random.randint(1,1000) # generate a random number between 1 and 1000
+    print(random_number) # print random number to console
     client.publish(topic,random_number) # publish this number to the topic
-    time.sleep(10) # sleep for 10 seconds
+    time.sleep(2) # sleep for 2 seconds
--- src/subscribe.py
+++ src/subscribe.py
@@ -1,13 +1,13 @@
 import paho.mqtt.client as paho # import the mqtt library (pip3 install paho-mqtt)
 
-broker = "broker.hivemq.com" # broker address
-topic = "jbm-example" # topic name to subscribe to
+broker = "broker" # broker address
+topic = "examples/#" # topic name to subscribe to in this case we are interested in everything under examples. This
+                     # this will include the random_number topic that publish.py is calling to.
 
-def on_connect_callback(client,userdata,flags,rc): # define the on_connect callback method
-    print("connection callback")
+def on_connect_callback(client,userdata,flags,rc): # define the on_connect callback method    
     client.subscribe(topic) # subscribe to to the topic, best to do in on_connect so if connection is lost topic is added again
 
-client = paho.Client(client_id="subscriber-001-jbm-example", # subscriber id
+client = paho.Client(client_id="subscriber-001-example", # subscriber id
                         transport="tcp",  # the connection protocol
                         reconnect_on_failure= True # attempt to reconnect on failure 
                         )
@@ -15,9 +15,9 @@ client = paho.Client(client_id="subscriber-001-jbm-example", # subscriber id
 client.on_connect=on_connect_callback # assign callback method to on_connect handle
 client.connect(broker) # connect to the broker
 
-def on_message(client,userdata,message):
+def on_message(client,userdata,message):    
     message_data = str(message.payload.decode('utf-8')) # decode the message that was recieved
-    print(f"message = {message_data}") # print the message payload
+    print(f"{message.topic} - {message_data}") # print the message payload to console
 
 client.on_message = on_message # assign the on_message_callback method to the on_message handle
 client.loop_forever()  # start networking daemon
